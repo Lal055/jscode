6a717dc054f8b40bc1b710075078bed5e19608ea
Lal055 <158134621+Lal055@users.noreply.github.com>
2024-02-12 11:43:32 +0000

    save

---
+++ basics/test1.js
@@ -0,0 +1,10 @@
+const accountId=1
+let accountmail="lal@lal.com"
+var accountpass="12345678"
+accountcity="jaipur"
+
+console.log(accountId);
+console.table([accountId,accountmail,accountpass,accountcity])
+/*
+prefer to use let not var because of block scope and functional scope
+*/
